d1dd65656935a01ab6315f5eabf98603de17b4d1
Bohdan <snizinkavolshebna@gmail.com>
2024-01-23 02:01:29 +0200

    Server

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000
+  },
   plugins: [
     vue(),
   ],
